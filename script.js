f7bb8f34c7f1bc3832fd148f25d97714f3eb9f09
Jake Kuo <96745417+Quasistarsupernova@users.noreply.github.com>
2025-11-27 04:30:17 +0000

    stuff

--- script.js
+++ script.js
@@ -2,30 +2,95 @@ let article = [
     [
         "你好世界！",
         `
-        你好，欢迎来到我的网站！在这边我会写一下我的日记，分享一些有趣的事情。
+            你好，欢迎来到我的网站！在这边我会写一下我的日记，分享一些有趣的事情。
         今天我去了公园，天气非常好，阳光明媚，微风轻拂。我看到很多人在跑步、骑自行车，还有一些人在草地上野餐。孩子们在游乐场玩耍，笑声充满了整个公园。
-        我还遇到了一只可爱的狗狗，它非常友好，摇着尾巴向我跑来。我和它玩了一会儿，感觉非常开心。
+        我还遇到了一只可爱的狗狗，它非常友好，摇着尾巴向我跑来我和它玩了一会儿，感觉非常开心。
         总的来说，这是一个美好的一天，我很享受在公园的时光。希望以后还能有更多这样的日子！
-        `
+        `,
+        ["https://s3-us-west-1.amazonaws.com/ehq-production-us-california/f40c5f18b3a36e49a6f5d3dd1d9526ce36e91002/original/1690314651/a70e88521a19164d83f4e559b6474274_thumbnail_image1.jpg?1690314651"],
     ],
     [
         "Example",
-        "Welcome to my website, ermmm idk what to put here lmao!"
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
+    ],
+    [
+        "Example",
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
+    ],
+    [
+        "Example",
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
+    ],
+    [
+        "Example",
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
+    ],
+    [
+        "Example",
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
+    ],
+    [
+        "Example",
+        "Welcome to my website, ermmm idk what to put here lmao!",
+        [],
     ],
 ];
+
+let off = 0;
+
+create();
 
 restart_.addEventListener('click', function(){
     create();
 });
 
+function load_(i){
+    articles_.innerHTML += `
+        <div `+`id="article`+i+`_" class="article">`+`
+            <div id="article_header">
+                <h2>`+article[i][0]+`</h2>
+            </div>
+            <p>`+article[i][1]+`</p>
+        </div>
+    `;
+
+    for(let j = 0; j < article[i][2].length; j++) {
+        document.getElementById("article"+j+"_").innerHTML += `<img src="`+article[i][2][j]+`" alt="Article Image `+j+`" class="article-image" height ="300px">`;
+    }
+}
+
 function create() {
     articles_.innerHTML = "";
-    for(let i = 0; i < article.length; i++) {
-        articles_.innerHTML += `
-            <div `+`id="article`+i+`_" class="article">`+`
-                <h2>`+article[i][0]+`</h2>
-                <p>`+article[i][1]+`</p>
-            </div>
-        `;
+    for(let i = 0; i < 3; i++) {
+        load_(i);
     }
+    off = 3;
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 5) {
+        setTimeout(() => {
+            extend();
+        }, 1000); 
+    }
+});
+
+
+function extend(){
+    if(off+3 > article.length){
+        for(let i = off; i < article.length; i++) {
+            load_(i);
+        }
+        off = article.length;
+        return;
+    }
+    for(let i = off; i < off+3; i++) {
+        load_(i);
+    }
+    off += 3;
 }
